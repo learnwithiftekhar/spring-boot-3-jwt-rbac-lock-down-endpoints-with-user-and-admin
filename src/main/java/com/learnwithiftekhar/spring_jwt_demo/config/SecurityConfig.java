b27f4d71cac216e94547be836a1a3a059c6ce03d
Iftekhar Hossain <learnwithiftekhar@gmail.com>
2025-06-12 13:08:24 +0600

    add session policy in spring security

--- src/main/java/com/learnwithiftekhar/spring_jwt_demo/config/SecurityConfig.java
+++ src/main/java/com/learnwithiftekhar/spring_jwt_demo/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -61,6 +62,9 @@ public class SecurityConfig {
                                 .anyRequest().authenticated()
                 ).oauth2ResourceServer(
                         rs -> rs.jwt(Customizer.withDefaults())
+                ).sessionManagement(
+                        session -> session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .build();
     }
